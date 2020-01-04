16e9f204ab9d23d2a6abb312b1ba1d3c51f1c76b
Sofiyyatul Husna Bte Habeeb Mohamed <husna.habeeb04@gmail.com>
2020-01-04 10:25:51 +0800

    new js file vue code inside

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      stockImages:[
+        {
+          title:"",
+          price: 0,
+          img: "",
+          tag: "",
+        },
+        {
+            title:"",
+            price: 0,
+            img: "",
+            tag: "",
+          },
+          {
+            title:"",
+            price: 0,
+            img: "",
+            tag: "",
+          }
+      ],
+      message:"hello world",
+    }
+  })
