77b9047ff962b03eb2fabb8d21521c06605bc589
Sofiyyatul Husna Bte Habeeb Mohamed <husna.habeeb04@gmail.com>
2020-01-04 12:50:02 +0800

    vue loop all the pics

--- js/main.js
+++ js/main.js
@@ -1,26 +1,33 @@
 var app = new Vue({
     el: '#app',
     data: {
-      stockImages:[
-        {
-          title:"",
-          price: 0,
-          img: "",
-          tag: "",
-        },
-        {
-            title:"",
-            price: 0,
-            img: "",
-            tag: "",
-          },
-          {
-            title:"",
-            price: 0,
-            img: "",
-            tag: "",
-          }
+        message:"jkjdlsd",
+        stockImages:[
+            {
+                title:"Lady in Coding",
+                price: 10,
+                img: "images/ss/6.jpg",
+                tag: "coding",
+            },
+            {
+                title:"Lady in Engineering",
+                price: 10,
+                img: "images/ss/3.jpg",
+                tag: "engineering",
+            },
+            {
+                title:"Lady in Business",
+                price: 10,
+                img: "images/ss/4.jpg",
+                tag: "business",
+            },
+            {
+                title:"Ladies in Business",
+                price: 10,
+                img: "images/ss/7.jpg",
+                tag: "business",
+            }
+            
       ],
-      message:"hello world",
     }
   })
