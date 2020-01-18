a2fbda19ef2e7c86b9f26751e33ea7c192b83018
Sofiyyatul Husna Bte Habeeb Mohamed <husna.habeeb04@gmail.com>
2020-01-18 11:30:30 +0800

    vrg

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var app = new Vue({
     el: '#app',
     data: {
-        stockImages:[{}],
+        stockImages:[
+            {"img":"https://firebasestorage.googleapis.com/v0/b/success-looks-like-this.appspot.com/o/codette-shutterstock%2Flady-engineering.jpg?alt=media&token=1e6c7b85-02ed-4551-8285-02aea6682b34","price":10,"tag":"engineering","title":"Lady in Engineering"},
+        ],
         isLoading:false
     },
     created: function () {
@@ -10,12 +12,15 @@ var app = new Vue({
     },
     methods: {
         populateFromDb: function () {
+            const stockImagesFromDb = []
             db.collection("stockImages").get().then((querySnapshot) => {
                 this.isLoading=true;
                 querySnapshot.forEach((doc) => {
-                    this.stockImages.push(doc.data())
+                    stockImagesFromDb.push(doc.data())
                 });
             }).then(()=> {
+                this.stockImages = [...this.stockImages, stockImagesFromDb];
+                console.log("this.stockImages", JSON.stringify(this.stockImages))
                 this.isLoading=false;
             });
         }
